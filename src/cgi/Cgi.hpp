37535dd475145d0046022a8875a1646b4a551c4a
qwww-12 <laro1laro34@gmail.com>
2026-05-16 21:54:22 +0100

    update cgi and fix code

--- src/cgi/Cgi.hpp
+++ src/cgi/Cgi.hpp
@@ -8,30 +8,21 @@
 #include <sys/wait.h>
 #include <vector>
 #include <map>
-#include "../http_parser/Parser.hpp"
-
-/*
-If your form has METHOD="POST" in its FORM tag, your CGI program will receive the encoded form input on stdin. The server will NOT send you an EOF on the end of the data,
-instead you should use the environment variable CONTENT_LENGTH to determine how much data you should read from stdin.
-*/
+#include "../main.hpp"
 
 class   Cgi {
-    private:
-        std::map<std::string, std::string>  cgiMap;
-        HttpRequest                         &req;
-        char                                **ev;
-        char                                **av;
-        int                                 outPipe[2];
-        int                                 inPipe[2];
+	private:
+		std::map<std::string, std::string>	&cgiMap;
+		HttpRequest							&structRequest;
+		int									outPipe[2];
+		int									inPipe[2];
+		std::string					getRequestMethod( HttpMethod requestMethod ) const;
+		void						closePipe( void );
+		std::vector<std::string>	buildEnvp( void );
+		std::string					findRunner( void ) const;
 
-        std::string     eMethod( HttpMethod mtd ) const;
-        int             getCONTENT_LENGTH( ) const;
-        void            closePipe( );
-        void            buildEnv( );
-        void            buildCmd( );
-
-    public:
-        Cgi( HttpRequest &req, std::map<std::string, std::string> &initCgiMap );
-        ~Cgi( );
-        std::string executeCGI( );
+	public:
+		Cgi( std::map<std::string, std::string> &initCgiMap, HttpRequest &initStructRequest );
+		~Cgi( );
+		std::string executeCGI( void );
 };
